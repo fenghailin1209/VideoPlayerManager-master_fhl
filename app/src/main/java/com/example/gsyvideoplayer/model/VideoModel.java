5791903595f657d4b9eca5fd5f2c7ab3293336ab
冯海林 <383272680@qq.com>
2017-11-07 10:50:40 +0800

    初始版本，将VideoPlayerManager里面的工具方法list-visibility-utils加了进去。

---
+++ app/src/main/java/com/example/gsyvideoplayer/model/VideoModel.java
@@ -0,0 +1,17 @@
+package com.example.gsyvideoplayer.model;
+
+/**
+ * Created by shuyu on 2016/11/11.
+ */
+
+public class VideoModel {
+    private boolean isAutoPlay;
+
+    public boolean isAutoPlay() {
+        return isAutoPlay;
+    }
+
+    public void setAutoPlay(boolean autoPlay) {
+        isAutoPlay = autoPlay;
+    }
+}
